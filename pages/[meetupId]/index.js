9a90cfa08a81686c249a2e397ba01ff55d05c938
nikosid <nikosid@gmail.com>
2021-10-13 12:50:50 +0200

    fix mongodb credentials

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -22,7 +22,7 @@ const DetailMeetupPage = (props) => {
 
 export async function getStaticPaths() {
   const client = await MongoClient.connect(
-    'mongodb+srv://maximilian:TU6WdZF2EjFWsqUt@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority'
+    'mongodb+srv://nextjsmeetups:rEdHrrOHnLCGZzAd@nextjs-cluster.yuge0.mongodb.net/meetups?retryWrites=true&w=majority'
   );
   const db = client.db();
 
